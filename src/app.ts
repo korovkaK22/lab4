f811209fb867494773ff2d360b303d340c6697a6
Andrii Sereda <sa0959328930@gmail.com>
2024-06-09 19:28:27 +0300

    docker fix one love linux :love:

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import mongoose, { ConnectOptions } from 'mongoose';
 import { log4jsconfig } from "./config/log4jsconfig";
 import * as process from "process";
 import { configDotenv } from "dotenv";
+import  {Request, Response} from 'express'
 
 configDotenv();
 
@@ -18,19 +19,19 @@ export default async () => {
 
   app.use(express.json({ limit: '1mb' }));
 
-  app.use('/api', routers);
-
   app.use((req,res,next)=>{
     res.status;
     log4js.getLogger().info(`Receiving ${req.method} request for ${req.url}`);
     next();
-
   });
 
+  app.get('/', (req: Request,res: Response)=>{req.body;res.status(200).json('Hello!')});
+  app.use('/api', routers);
+
+
   const port = Number(process.env.PORT) || 3000;
-  const address = process.env.HOST || 'localhost';
-  app.listen(port, address, () => {
-    log4js.getLogger().info(`Example app listening on port ${address}:${port}`);
+  app.listen(port, () => {
+    log4js.getLogger().info(`Example app listening on port ${port}`);
   });
 
   const mongoAddress = process.env.MONGODB_URL || "";
@@ -39,6 +40,7 @@ export default async () => {
     useUnifiedTopology: true,
     socketTimeoutMS: 30000,
   } as ConnectOptions);
+  console.log('db connected')
 
   return app;
 };
